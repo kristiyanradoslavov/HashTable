bea5946d2b78e6f460c469cc0c0686fe05b372ea
Kristiyan Radoslavov <radoslavov48@gmail.com>
2022-12-29 23:42:38 +0200

    new change

---
+++ test_hash_table.py
@@ -0,0 +1,22 @@
+from unittest import TestCase, main
+
+from HashTable import HashTable
+
+
+class TestHashTable(TestCase):
+
+    def setUp(self):
+        self.table = HashTable()
+
+    def test_initialising(self):
+        self.assertEqual(4, self.table.capacity)
+        self.assertEqual([None, None, None, None], self.table.keys)
+        self.assertEqual([None, None, None, None], self.table.values)
+
+    def test_getitem_wrong_input(self):
+        with self.assertRaises(KeyError) as ke:
+            result = self.table["something_missing"]
+
+
+if __name__ == '__main__':
+    main()
